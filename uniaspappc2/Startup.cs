131434a9e7bcd65ab50a8d4578f827d4499a0861
unifent <unifent@gmail.com>
2015-01-02 17:25:08 +0100

    load apop

---
+++ uniaspappc2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(uniaspappc2.Startup))]
+namespace uniaspappc2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
